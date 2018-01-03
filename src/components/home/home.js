d843dc194a4d8bc28bfcf09055a72a68e90cafd1
Kang Kang1 Chen <chenkang1@Lenovo.com>
2018-01-03 15:08:13 +0800

    add collapse

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CustomSlider from "../slider/slider";
-
+// import Collapse from "../collapse/collapse";
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
         <CustomSlider />
+        {/* <Collapse /> */}
       </div>
     );
   }
