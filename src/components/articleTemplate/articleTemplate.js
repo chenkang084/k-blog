10320f9daed057850a87ef67883ef3c79ef4242c
Kang Kang1 Chen <chenkang1@Lenovo.com>
2018-01-08 14:51:38 +0800

    add modal

---
+++ src/components/articleTemplate/articleTemplate.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./articleTemplate.less";
+// import { ModalContainer, ModalDialog } from "../modal";
+// import { ModalContainer, ModalDialog } from "react-modal-dialog";
+import KModal from "../kModal";
+import ModalDialog from "../kModal/modalDialog";
+
+export default class ArticleTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = { isShowingModal: false };
+  handleClick = () => this.setState({ isShowingModal: true });
+  handleClose = () =>
+    this.setState({
+      isShowingModal: false
+    });
+
+  render() {
+    const modalProps = {
+      handleClick: this.handleClick,
+      handleClose: this.handleClose,
+      isShowingModal: this.state.isShowingModal
+    };
+
+    return (
+      <div>
+        <button onClick={this.handleClick}>btn</button>
+        <KModal {...modalProps}>test</KModal>
+      </div>
+    );
+  }
+}
